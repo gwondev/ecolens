d5e1124ba2c8f944089d990bc50c585cbc5869fb
gwon <gwondev0323@gmail.com>
2026-03-25 16:45:42 +0900

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootPage from "./pages/RootPage";
 import DBPage from "./pages/DBPage";
+import NicknamePage from "./pages/NicknamePage"; // 추가됨
+import MapPage from "./pages/MapPage";           // 추가됨
 import Recognition from "./pages/features/Recognition";
 import Reward from "./pages/features/Reward";
 import Control from "./pages/features/Control";
@@ -10,9 +12,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 시작 및 계정 설정 */}
         <Route path="/" element={<RootPage />} />
+        <Route path="/nickname" element={<NicknamePage />} />
+
+        {/* 메인 서비스 (지도) */}
+        <Route path="/map" element={<MapPage />} />
+
+        {/* 관리자 전용 페이지 */}
         <Route path="/db" element={<DBPage />} />
 
+        {/* 개별 기능 페이지 */}
         <Route path="/features/recognition" element={<Recognition />} />
         <Route path="/features/guide" element={<Guide />} />
         <Route path="/features/reward" element={<Reward />} />
